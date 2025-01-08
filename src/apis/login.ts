627f6652f18e254919158c0c2fcfebfdc8cd4e1e
b1g-Dr4goN <kuanmin.bigdragon.56@gmail.com>
2025-01-08 23:05:05 +0700

    change backend domain name and login headers

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -2,7 +2,7 @@ import axiosInstance from "../utils/axiosInstance";
 
 const axiosLogin = async (usr: string, pass: string): Promise<string> => {
     try {
-        const res = await axiosInstance.post(`/auth/login?userName=${usr}&password=${pass}`);
+        const res = await axiosInstance.post(`/auth/login`, { userName: usr, password: pass });
         return res.data.split(" ").map((val: string) => val)[2];
     } catch (err) {
         console.log(err);
